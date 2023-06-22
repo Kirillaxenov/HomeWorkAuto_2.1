c94482edd1829e257381bd1b59190b5ca3b971ea
Kirill <kaxyonov@gmail.com>
2023-06-22 13:40:25 +0300

    redoing tests

--- src/test/java/org/example/CardApplicationTest.java
+++ src/test/java/org/example/CardApplicationTest.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-public class cardApplicationTest {
+public class CardApplicationTest {
     private WebDriver driver;
 
 
@@ -28,6 +28,8 @@ public class cardApplicationTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
+
     }
 
     @AfterEach
@@ -37,8 +39,7 @@ public class cardApplicationTest {
     }
 
     @Test
-    void shouldSomethingTest() throws InterruptedException {
-        driver.get("http://localhost:9999/");
+    void shouldSomethingTest() {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+78888888888");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
@@ -46,14 +47,12 @@ public class cardApplicationTest {
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals(expected, actual);
-//        Thread.sleep(5000);// Задержка,чтобы брауер не сразу закрывался
 
 
     }
 
     @Test
-    void shouldWrongNameTest() throws InterruptedException {
-        driver.get("http://localhost:9999/");
+    void shouldWrongNameTest() {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ivanov Ivan");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+78888888888");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
@@ -66,13 +65,10 @@ public class cardApplicationTest {
     }
 
     @Test
-    void shouldNothingNameTest() throws InterruptedException {
-        driver.get("http://localhost:9999/");
+    void shouldNothingNameTest() {
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
         driver.findElement(By.cssSelector("button")).click();
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-
-
         String expected = "Поле обязательно для заполнения";
         String actual = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
         assertEquals(expected, actual);
@@ -81,9 +77,7 @@ public class cardApplicationTest {
     }
 
     @Test
-    void shouldNothingPhoneTest() throws InterruptedException {
-        driver.get("http://localhost:9999/");
-
+    void shouldNothingPhoneTest() {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("button")).click();
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
@@ -94,13 +88,11 @@ public class cardApplicationTest {
     }
 
     @Test
-    void shouldLongPhoneTest() throws InterruptedException {
-        driver.get("http://localhost:9999/");
+    void shouldLongPhoneTest() {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+797876755544544");
         driver.findElement(By.cssSelector("button")).click();
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-
         String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
         assertEquals(expected, actual);
@@ -108,16 +100,5 @@ public class cardApplicationTest {
 
     }
 
-    @Test
-    void shouldNotCheckBoxTest() throws InterruptedException {
-        driver.get("http://localhost:9999/");
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79888888888");
-        driver.findElement(By.cssSelector("button")).click();
-        String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid .checkbox__text")).getText().trim();
-        assertEquals(expected, actual);
 
-
-    }
 }
